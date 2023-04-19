9f92f628792b8a33d1c586d2bee73d74cb079a81
nbss1 <130272394+nbss1@users.noreply.github.com>
2023-04-19 17:20:37 +0545

    Update update_time.js

--- js/update_time.js
+++ js/update_time.js
@@ -1,7 +1,10 @@
-      window.onload = function() {
-        var date = new Date(document.lastModified);
-        var options = { year: 'numeric', month: 'long', day: 'numeric', weekday: 'long', era: 'short', hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true };
-        var nepaliDate = date.toLocaleDateString('ne-NP-u-ca-perso', options);
-        var lastUpdate = document.getElementById('last-update');
-        lastUpdate.textContent = nepaliDate;
-      }
+window.onload = function() {
+    var date = new Date();
+    var options = { year: 'numeric', month: 'long', day: 'numeric', weekday: 'long', era: 'short', hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: false, timeZone: 'Asia/Kathmandu', calendar: 'nepali' };
+    var nepaliDate = date.toLocaleDateString('ne-NP', options);
+    var dayOfWeek = date.toLocaleDateString('ne-NP', { weekday: 'long', timeZone: 'Asia/Kathmandu', calendar: 'nepali' });
+    var time = date.toLocaleTimeString('ne-NP', { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: false, timeZone: 'Asia/Kathmandu' });
+    var fullDate = nepaliDate + ', ' + dayOfWeek + ', ' + time;
+    var nepaliTime = document.getElementById('nepali-time');
+    nepaliTime.textContent = fullDate;
+}
