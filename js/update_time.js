15acbeeb23648e4568da7c603f9a8d6b659924f1
nbss1 <130272394+nbss1@users.noreply.github.com>
2023-04-19 17:32:20 +0545

    Update update_time.js

--- js/update_time.js
+++ js/update_time.js
@@ -1,10 +1,19 @@
-window.onload = function() {
-    var date = new Date();
-    var options = { year: 'numeric', month: 'long', day: 'numeric', weekday: 'long', era: 'short', hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: false, timeZone: 'Asia/Kathmandu', calendar: 'nepali' };
-    var nepaliDate = date.toLocaleDateString('ne-NP', options);
-    var dayOfWeek = date.toLocaleDateString('ne-NP', { weekday: 'long', timeZone: 'Asia/Kathmandu', calendar: 'nepali' });
-    var time = date.toLocaleTimeString('ne-NP', { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: false, timeZone: 'Asia/Kathmandu' });
-    var fullDate = nepaliDate + ', ' + dayOfWeek + ', ' + time;
-    var nepaliTime = document.getElementById('nepali-time');
-    nepaliTime.textContent = fullDate;
-}
+      window.onload = function() {
+        var lastUpdate = new Date(document.lastModified);
+        var options = { 
+          year: 'numeric', 
+          month: 'long', 
+          day: 'numeric', 
+          weekday: 'long', 
+          era: 'short', 
+          hour: 'numeric', 
+          minute: 'numeric', 
+          second: 'numeric', 
+          hour12: false, 
+          timeZone: 'Asia/Kathmandu', 
+          calendar: 'nepali' 
+        };
+        var nepaliDate = lastUpdate.toLocaleDateString('ne-NP', options);
+        var updateMessage = document.getElementById('last-update');
+        updateMessage.textContent = "Last updated on " + nepaliDate;
+      };
