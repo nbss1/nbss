ae19404c6bc758ff64a1532c5187796b3956273c
nbss1 <130272394+nbss1@users.noreply.github.com>
2023-04-21 14:20:16 +0545

    Delete last_update.js

--- js/last_update.js
+++
@@ -1,5 +0,0 @@
-const lastModifiedElement = document.getElementById("last-modified");
-const lastModified = new Date(document.lastModified);
-const bsDate = new NepaliDate(lastModified);
-const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', timeZoneName: 'short' };
-lastModifiedElement.textContent = `Last modified on ${bsDate.toLocaleDateString('ne-NP', options)}`;
